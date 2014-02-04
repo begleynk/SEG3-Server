24b16b615110ed256e4d151544d72abfd2c4a7ed
James Bellamy <k1209605@kcl.ac.uk>
2014-02-04 21:40:25 +0000

    Enesay

--- src/FooMar.java
+++ src/FooMar.java
@@ -2,9 +2,7 @@
  * Created by Me on 04/02/2014.
  */
 public class FooMar {
-
-    public static void main(String[] args)
-    {
-        System.out.println("Foo in the bar");
+    public static void main(String[] args) {
+        System.out.println("Enesay");
     }
 }
