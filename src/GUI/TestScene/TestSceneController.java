a13da88bf98ef250a9a93f40193f6265f0ba1d6e
James Bellamy <k1209605@kcl.ac.uk>
2014-02-27 12:31:36 +0000

    Starting to try switching panes in and out

---
+++ src/GUI/TestScene/TestSceneController.java
@@ -0,0 +1,16 @@
+package GUI.TestScene;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+/**
+ * Created by Me on 27/02/2014.
+ */
+public class TestSceneController extends Scene {
+
+    public TestSceneController(Parent parent) {
+        super(parent);
+
+
+    }
+}
