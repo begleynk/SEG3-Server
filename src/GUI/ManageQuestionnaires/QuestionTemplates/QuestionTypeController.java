2bd540560b97d124663818c87c6e1da5aff6e082
fe7 <faizanjoya@gmail.com>
2014-03-09 12:29:53 +0000

    GUI: Question templates for question types: FreeText, MultipleChoice, Range, Rank, SingleChoice.

--- src/GUI/ManageQuestionnaires/QuestionTemplates/QuestionTypeController.java
+++ src/GUI/ManageQuestionnaires/QuestionTemplates/QuestionTypeController.java
@@ -3,6 +3,8 @@ package GUI.ManageQuestionnaires.QuestionTemplates;
 /**
  * Created by Faizan Joya on 09/03/14.
  * SEG3-Server
+ *
+ * work in progress
  */
 public class QuestionTypeController {
 }
