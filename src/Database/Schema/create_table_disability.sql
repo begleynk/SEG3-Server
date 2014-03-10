e6d8a7e418443cfede1e6e09d59ba62158f6d75a
Sekban <semihersahin@gmail.com>
2014-03-10 10:49:00 +0000

    Updated the database with log tables for patient and questionnaire

--- src/Database/Schema/create_table_disability.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS 'Disability' (
-  'P_disability' varchar(20) PRIMARY KEY NOT NULL
-);
--- src/Database/Schema/create_table_patient_disability.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS 'Patient_Disability' (
-  'P_disability' varchar(20) NOT NULL,
-  'P_NHS_number' int(10) NOT NULL,
-  PRIMARY KEY('P_NHS_number', 'P_disability'),
-  FOREIGN KEY('P_NHS_number') REFERENCES 'Patient'('P_NHS_number'),
-  FOREIGN KEY('P_disability') REFERENCES 'Disability'('P_disability')
-  );
